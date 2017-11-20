2d94e58a16531dc761bf518b446c759c5095a1df
FlashNoob98 <danieledaniele1988@gmail.com>
2017-11-20 17:01:10 +0100

    verifica input e miglioria output

--- caduta_grave/caduta_di_un_grave.cpp
+++ caduta_grave/caduta_di_un_grave.cpp
@@ -1,19 +1,29 @@
-/* Caduta di un grave */
+/* caduta_grave.cpp */
 #include <iostream>
 using namespace std;
 int main()
 {
-	float h,g=9.81,t,delta,x;
-	cout << "Inserisci l'altezza dalla quale cade il grave: ";
-	cin >> h;
-	cout << "Insrisci il passo del calcolo: ";
-	cin >>delta;
-	while (x>0)
+	double h,t,delta,x;
+	const float g = 9.80665;
+	cout << "Con questo programma puoi calcolare la posizione di un grave"<< endl;
+	cout << "in caduta libera in assenza di attrito, con step arbitrario" << endl;
+	do{
+		cout << "Entrambi i dati devono essere positivi"<< endl; //input e verifica dati
+		cout << "Inserisci l'altezza: ";
+		cin >> h;
+		cout << "Insrisci lo step: ";
+		cin >> delta;
+		cout << endl;
+		}
+	while(!((h > 0) && (delta > 0)));
+	cout << endl << "Tempo	Posizione" <<endl;
+	t = delta;
+	while (x > 0)
 	{
-		x=h-0.5*g*t*t;
+		x = h - (0.5*g*t*t); //calcolo posizione
 		cout.width(4);
-		cout <<"T:"<<t<<" s  "<<x<<" m"<<endl;
-		t = t+delta;
+		cout << t <<"s   "<< x <<"m"<< endl; //output
+		t = t + delta; //incremento step
 	}
 	return 0;
 }
