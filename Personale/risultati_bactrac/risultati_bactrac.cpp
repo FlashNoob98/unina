3b106fedda59d000815d919b077426471e8c8bed
FlashNoob98 <danieledaniele1988@gmail.com>
2017-12-11 21:19:39 +0100

    aggiunto output su file

--- Personale/risultati_bactrac/risultati_bactrac.cpp
+++ Personale/risultati_bactrac/risultati_bactrac.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <math.h>
-#include <ostream>
+#include <cmath>
+#include <fstream>
 #include <string>
 
 using namespace std;
@@ -25,9 +25,27 @@ double input_numero(){
 		return n;
 }
 
+int print_res(double a,double b, string res){
+	//debug cout <<a<<" + "<<b<<" = "<<res<<endl;
+	ofstream file ("Risultati.txt",ofstream::app);
+  if (file.is_open())
+  {
+	  file.width(8);
+	  file << a;
+	  //file.width(8);
+      file <<" and ";
+      file.width(8);
+      file <<b<<" = "<<res<<endl;
+      file.close();
+  }
+  else cout <<endl<< "Impossibile aprire il file";
+  
+	return 0;
+	}
+
 int main(){
 	cout << "Inserisci i valori o q per uscire"<<endl;
-	double r,s,risultato;
+	double r,s,risultato=0;
 	while(true){
 		cout << "Inserisci il primo numero: ";
 		r = input_numero();
@@ -35,11 +53,14 @@ int main(){
 		s = input_numero();
 		if(abs(log10(r/s))<1){
 			cout << "Valore non accettabile, nullo."<<endl<<endl;
+			print_res(r,s,"nullo"); //stampa su file errore
 		}
 		else{
 			risultato=(pow((abs(r*s)),0.5))*100;	//media geometrica ecc
 			cout << "Il risultato e': "<<risultato<<endl<<endl;
-		}
+			print_res(r,s,std::to_string(risultato)); //stampa su file risultato
+		}		
+		
 	}
 	return 0;
 }
