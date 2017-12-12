c1d8d46716f61448cb35db31a564d01a6d93455e
FlashNoob98 <danieledaniele1988@gmail.com>
2017-12-12 17:44:15 +0100

    variabili inutili rimosse

--- Array_matrici_stringhe/somma_array/somma_array.cpp
+++ Array_matrici_stringhe/somma_array/somma_array.cpp
@@ -4,20 +4,20 @@
 using namespace std;
 int main()
 {
-	int righe=r;
-	int colonne=c;
-	int m[righe][colonne]={0};
+	//int righe=r;
+	//int colonne=c;
+	int m[r][c]={0};
 	
-	for (int i=0; i<righe-1; i++){			//input
-		for (int j=0; j<colonne; j++){
+	for (int i=0; i<r-1; i++){			//input
+		for (int j=0; j<c; j++){
 			cout << "Inserisci elemento riga "<<i+1<<" colonna "<<j+1<<": ";
 			cin >> m[i][j];
-			m[righe-1][j]+=m[i][j]; //calcolo
+			m[r-1][j]+=m[i][j]; //calcolo
 		}
 	}
 	
-	for (int i=0; i<righe; i++){			//output
-		for (int j=0; j<colonne; j++){
+	for (int i=0; i<r; i++){			//output
+		for (int j=0; j<c; j++){
 			cout.width(3);
 			cout << m[i][j];
 		}
@@ -25,4 +25,3 @@ int main()
 	}
 	return 0;
 }
-
