c3aad3b92e5e1026e87e7f79fae21abe9a968758
Daniele Olivieri <danieledaniele1988@gmail.com>
2017-12-12 15:48:08 +0100

    crivello di eratostene nuovo

---
+++ Aritmetica/crivello_eratostene/crivello_eratostene.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout << "Inserisci dimensione array: ";
+	cin >> n;
+	bool set[n+1]={0};
+	for (int i = 2;i<=n;i++){
+		if (set[i])
+			continue;
+		for(int j=2*i;j<=n;j+=i){
+				set[j] = true;
+		}
+		
+	}
+	for (int j=1;j<n;j++){
+		if (!set[j]){
+			cout.width(4);
+			cout << j;
+		}
+	}
+	return 0;
+}
+
