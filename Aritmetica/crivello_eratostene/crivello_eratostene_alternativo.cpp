35cc4f3c8a63b318b6562f56989021e59f5355a3
Daniele Olivieri <danieledaniele1988@gmail.com>
2017-12-12 17:17:30 +0100

    eliminato un ciclo for

--- Aritmetica/crivello_eratostene/crivello_eratostene_alternativo.cpp
+++ Aritmetica/crivello_eratostene/crivello_eratostene_alternativo.cpp
@@ -9,21 +9,30 @@ int main()
 
 	for (int i = 1;i<n;i++){
 		if (set[i+1]) //numero già valutato, salto il ciclo //utilizzo i+1 per usare array della stessa dimensione del setaccio
-			continue;
+			goto jump;
 		for(int j=(2*i)+1;j<n;j++){
 				if(j%(i+1)==0) //condizione modulo per divisibilità
 				set[j] = true;
 		}
+		jump:
+		if (!((i)%10))
+			cout <<endl;
+			
+		if ((!set[i])&&i>1){
+			cout.width(6);
+			cout << i;
+		}
+
 	}
 
-	for (int j=2;j<n;j++){ //output
+	/*for (int j=2;j<n;j++){ //output
 		if (!set[j]){
 			cout.width(6);
 			cout << j;
 		}
 		if (!(j%10))
 			cout <<endl;
-	}
+	}*/
 	cout <<endl;
 
 	return 0;
