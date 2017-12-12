196b3fac2f4a34eaef5551956a9adc147c688e28
Daniele Olivieri <danieledaniele1988@gmail.com>
2017-12-12 17:00:20 +0100

    aggiunto crivello eratostene con funzione modulo

---
+++ Aritmetica/crivello_eratostene/crivello_eratostene_alternativo.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout << "Inserisci dimensione array: ";
+	cin >> n;
+	bool set[n]={0};
+
+	for (int i = 1;i<n;i++){
+		if (set[i+1]) //numero già valutato, salto il ciclo //utilizzo i+1 per usare array della stessa dimensione del setaccio
+			continue;
+		for(int j=2*i;j<n;j++){
+				if(j%(i+1)==0) //condizione modulo per divisibilità
+				set[j] = true;
+		}
+	}
+
+	for (int j=1;j<n;j++){ //output
+		if (!set[j]){
+			cout.width(6);
+			cout << j;
+		}
+		if (!(j%10))
+			cout <<endl;
+	}
+	cout <<endl;
+
+	return 0;
+}
+
