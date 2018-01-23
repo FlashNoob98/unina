b043d9f94a37199a5d849075385f2a494ced6bde
FlashNoob98 <danieledaniele1988@gmail.com>
2018-01-23 21:50:57 +0100

    aggiunti commenti

--- Array_matrici_stringhe/input_array_no_ripetizioni/input_array.cpp
+++ Array_matrici_stringhe/input_array_no_ripetizioni/input_array.cpp
@@ -12,16 +12,16 @@ int main(){
       clone = 0;
       cout << "Inserisci elemento "<<i+1<<" nell'array: ";
       cin >> n;
-      for (int k=0;k<dim;k++){
-        if (n==A[k])  clone = 1;
+      for (int k=0;k<dim;k++){ //verifico se per ogni elemento dell'array il valore coincide
+        if (n==A[k])  clone = 1; //se sì pongo clone a 1
       }
     if (clone) cout <<"Elemento già inserito"<<endl;
-    } while(clone);
-
+  } while(clone); //ripeto l'input se ho trovato un clone altrimenti
+                  //inserisco il valore nell'array e riprendo il ciclo
     A[i]=n;
   }
   cout <<"Ecco l'array:"<<endl;
-  for(int i=0;i<dim;i++){
+  for(int i=0;i<dim;i++){ //output array
     cout << A[i]<<" ";
   }
   cout <<endl;
