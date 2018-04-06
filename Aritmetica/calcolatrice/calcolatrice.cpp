2f77e86778e63eeb8e3b75595ba9b7e7c18a00a3
Daniele <danieledaniele1988@gmail.com>
2018-04-06 17:52:38 +0200

    aggiunta operazione Cleaning

--- Aritmetica/calcolatrice/calcolatrice.cpp
+++ Aritmetica/calcolatrice/calcolatrice.cpp
@@ -8,17 +8,24 @@ int main (){
 	cout<< "Inserisci '0' per terminare"<< endl;
 	cout<< "Inserisci numero: ";
 	cin>> partial;
-	while (operand!=0){
-		cout<< "Inserisci operazione (+,-,*,/): ";
+	while (true){
+		cout<< "Inserisci operazione (+,-,*,/,c): ";
 		cin>> operazione;
+		if (operazione == 'c'){
+			partial =0;
+			cout <<"Parziale: "<<partial<<endl;
+			continue;
+		}
 		if (operazione == '0') break;
 		cout << "Inserisci numero: ";
 		cin >> operand;
+		if (operand == 0) break;
 		switch (operazione){
 			case '+': partial=partial+operand;break;
 			case '-': partial=partial-operand;break;
 			case '*': partial=partial*operand;break;
 			case '/': partial=partial/operand;break;
+			//case 'c': partial=0;break;
 		}
 		cout<<"Parziale: "<<partial<<endl;
 	}
