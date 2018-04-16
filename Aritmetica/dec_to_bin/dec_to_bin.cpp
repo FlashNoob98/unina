641299495a21943fd84a765d009c465a434dcad4
Daniele Olivieri <danieledaniele1988@gmail.com>
2018-04-16 18:29:05 +0200

    Convertitore dec bin

--- Aritmetica/dec_to_bin/dec_to_bin.cpp
+++ Aritmetica/dec_to_bin/dec_to_bin.cpp
@@ -1,9 +1,26 @@
 /* convertitore decimale binario */
 #include <iostream>
+#include <cmath>
+#define bit 8
+
 using namespace std;
 int main(){
 	int num;
+	bool bin[bit]={0};
 	cout << "Questo programma esegue la conversione in binario di un dato numero inserito"<<endl;
-	
-	
+	cout << "Inserisci il numero da convertire (max "<<pow(2,bit)-1<<"): "<<endl;
+	cin >>num;
+	for (int i=0; i<bit;i++){
+		if (num%2!=0){
+			bin[bit-i-1]=1;
+		}
+		num = num/2;
 	}
+	
+	for (int i=0; i<bit;i++){
+		cout <<bin[i];
+	}
+	
+	cout <<endl;
+	return 0;
+}
