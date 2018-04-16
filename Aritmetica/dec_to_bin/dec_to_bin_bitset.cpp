28e149e935a02367d41a98ab96569868f915b80a
Daniele Olivieri <danieledaniele1988@gmail.com>
2018-04-16 20:25:42 +0200

    migliorato ancora output

--- Aritmetica/dec_to_bin/dec_to_bin_bitset.cpp
+++ Aritmetica/dec_to_bin/dec_to_bin_bitset.cpp
@@ -6,11 +6,12 @@ int main(){
 	cout << "Inserisci il numero fino al quale eseguire la conversione in binario: ";
 	cin >>n; 
     for (int i = 0; i<=n; i++){
-		if (i<256) 				cout <<i<<" "<< bitset<8>(i)<<endl;
-		else if (i<65536)		cout <<i<<" "<< bitset<16>(i)<<endl;
-		else if (i<16777216)	cout <<i<<" "<< bitset<24>(i)<<endl;
-		else if (i<4294967296)  cout <<i<<" "<< bitset<32>(i)<<endl;
-		else 					cout <<i<<" "<< bitset<64>(i)<<endl;
+		if (i<256) 					cout <<i<<" "<< bitset<8>(i)<<endl;
+		else if (i<65536)			cout <<i<<" "<< bitset<16>(i)<<endl;
+		else if (i<16777216)		cout <<i<<" "<< bitset<24>(i)<<endl;
+		else if (i<4294967296)  	cout <<i<<" "<< bitset<32>(i)<<endl;
+		else if (i<1099511627776)	cout <<i<<" "<< bitset<40>(i)<<endl;
+		else 						cout <<i<<" "<< bitset<64>(i)<<endl;
     }
-   return 0;
+	return 0;
 }
