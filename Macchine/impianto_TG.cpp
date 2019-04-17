af8829c77e05c3530444db7cc763e74fed296af9
Daniele Olivieri <danieledaniele1988@gmail.com>
2019-04-17 18:30:04 +0200

    nuovo file, calcolatore TG

---
+++ Macchine/impianto_TG.cpp
@@ -0,0 +1,32 @@
+//commento
+#include <iostream>
+using namespace std;
+float T2,T3,T4,P2,P3,P4,Beta,Eta,mdot,power;
+float T1=15,P1=1,k=1.4,mass=28.9; //aria ISO
+	
+void input(){
+	cout << "ciao"<<endl;
+	return;
+}	
+int main(){
+	cout << "Questo è un calcolatore dei valori caratteristici"<<endl;
+	cout << "di un impianto con turbina a Gas semplice"<<endl;
+	cout<<endl;
+	cout<<"        --→→--(CC)--→→--     " << endl;
+	cout<<"       |(2)          (3)|    " << endl;
+	cout<<"    |\\ |                | /|      ___" << endl;
+	cout<<"    | \\|                |/ |     /" << endl;
+	cout<<"    |C |================|TG|==(∼)---- " << endl;
+	cout<<"    | /                  \\ |     \\___" << endl;
+	cout<<"    |/                    \\| " << endl;
+	cout<<"    |                      | " << endl;
+	cout<<"    ↑                      ↓ " << endl;
+	cout<<"    ↑(1)                (4)↓ " << endl;
+	cout<<"    |                      | " << endl<<endl;
+	
+	
+	
+	input();
+	
+	return 0;
+}
