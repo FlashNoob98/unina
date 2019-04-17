f0d434e866a1c2d57edf5520f70cea9b26cbdbae
Daniele Olivieri <danieledaniele1988@gmail.com>
2019-04-17 19:37:26 +0200

    aggiunti valori input

--- Macchine/impianto_TG.cpp
+++ Macchine/impianto_TG.cpp
@@ -1,42 +1,16 @@
 /* Programma per il calcolo dei valori caratteristici di un impianto semplice con turbina a gas
+ * Fornito senza alcuna garanzia e per uso non professionale, libera la copia, la modifica e la ridistribuzione
  * Scritto da Olivieri Daniele
- * Fornito senza alcuna garanzia e per uso non professionale, libera la copia, la modifica e la ridistribuzione del seguente
- * 
  */
 #include <iostream>
 using namespace std;
 float T2,T3,P2,P3,P4;  //Valori dell'acqua
-float Beta,Eta,mdot,power,T4; //Parametri impianto
-float T1=15,P1=1,k=1.4,mass=28.9; //atmosfera ISO
-	
-void input(){
-	char ans='N';
-	cout << "Questo programma utilizza i valori dell'atmosfera ICAO"<<endl;
-	cout << "Desideri modificarli? (s/N)";
-	cin >> ans;
-	if ((ans == 's') || (ans =='S')){ //parametri atmosfera ISO (facoltativo)
-		cout<<"Inserisci il valore di T1 (°C): ";
-		cin>>T1;
-		cout<<"Inserisci il valore di P1 (atm): ";
-		cin>>P1;
-		cout<<"Inserisci la costante k: ";
-		cin>>k;
-		cout<<"Inserisci la massa molare (Kg/Kmol): ";
-		cin>>mass;
-	}
-	cout<<endl<<"Ora devi inserire tutti i parametri della macchina"<<endl; //Parametri impianto
-	cout<<"Potenza (kW): "; cin >> power;
-	cout<<"Efficienza (%): "; cin >> Eta; Eta=Eta/100;
-	cout<<"Rapporto di compressione beta: "; cin >> Beta;
-	cout<<"Portata massica (kg/s): "; cin>>mdot;
-	cout<<"Temperatura in uscita dalla turbina T4 (°C): "; cin>>T4;
-	
-	return;
-}	
+float Beta,Eta_globale,mdot,power,T4; //Parametri impianto
+float T1=15,P1=1,k=1.4,mass=28.96,Cp=1.005; //atmosfera ISO
+float lambda;
 
 
-
-int main(){
+void schema(){
 	cout << "Questo è un calcolatore dei valori caratteristici"<<endl;
 	cout << "di un impianto con turbina a Gas semplice"<<endl;
 	cout<<endl;
@@ -52,10 +26,50 @@ int main(){
 	cout<<"    ↑                      ↓ " << endl;
 	cout<<"    ↑(1)                (4)↓ " << endl;
 	cout<<"    |                      | " << endl<<endl;
+	return;
+}
+
+void input(){
+	char ans='N';
+	cout << "Questo programma utilizza i valori dell'atmosfera ICAO"<<endl;
+	cout << "Desideri modificarli? (s/N) ";
+	cin >> ans;
+	if ((ans == 's') || (ans =='S')){ //parametri atmosfera ISO (facoltativo)
+		cout<<"Inserisci il valore di T1 ("<<T1<<" °C): ";
+		cin>>T1;
+		cout<<"Inserisci il valore di P1 ("<<P1<<" atm): ";
+		cin>>P1;
+		cout<<"Fattore di espansione isoentropico k ("<<k<<" gas biatomici): ";
+		cin>>k;
+		cout<<"Inserisci la massa molare ("<<mass<<" kg/kmol): ";
+		cin>>mass;
+		cout<<"Inserisci il Cp: ("<<Cp<<" kJ/(kg*K)): ";
+		cin>>Cp;
+	}
+	cout<<"Utilizzerò i seguenti valori: \nT1 = "<<T1<<"°C \nP1 = "<<P1<<" atm \nk = "<<k<<"  \nmassa = "<<mass<<" kg/kmol \nCp = "<<Cp<<" kJ/(kg*K)"<<endl;
+	cout<<endl<<"Ora devi inserire tutti i parametri della macchina"<<endl; //Parametri impianto
+	cout<<"Potenza (kW): "; cin >> power;
+	cout<<"Efficienza (%): "; cin >> Eta_globale; Eta_globale=Eta_globale/100;
+	cout<<"Rapporto di compressione beta: "; cin >> Beta;
+	cout<<"Portata massica (kg/s): "; cin>>mdot;
+	cout<<"Temperatura in uscita dalla turbina T4 (°C): "; cin>>T4;
 	
-	
-	
+	return;
+}	
+
+void calcolo_P2(){
+	P2=Beta*P1;
+	return;
+}
+
+
+
+int main(){
+	schema();
 	input();
+	calcolo_P2();
+	//cout <<P2;
+	lambda=(k-1)/k;
 	
 	return 0;
 }
